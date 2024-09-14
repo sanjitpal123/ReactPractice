c60022524529282b12ece108d9d2d879196cd25f
sanjitssp <manjitpal895@gmail.com>
2024-09-14 09:17:23 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+function App() {
+  const obj1 = [
+    { id: 1, heading: "hi", dec: "first" },
+    { id: 2, heading: "hello", dec: "second" },
+    { id: 3, heading: "helloth", dec: "third" }
+  ];
+
+  const [state, setState] = useState({});
+  const [check, SetChecked] = useState({});
+
+  function HandleDisplay(id) {
+    const f = obj1.find((item) => item.id === id);
+    setState(f || {});
+    
+    // Set the value to true for the selected item
+    SetChecked((prev) => ({ ...prev, [id]: true }));
+  }
+
+  return (
+    <div>
+      {obj1.map((item) => (
+        <div key={item.id}>
+          <div style={{ display: "flex", gap: "5px" }}>
+            {/* Use check[item.id] to determine if the radio button should be checked */}
+            <input type="radio" checked={check[item.id] === true} readOnly />
+            <div onClick={() => HandleDisplay(item.id)}>{item.heading}</div>
+            {item.id === state.id && <p>{state.dec}</p>}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
